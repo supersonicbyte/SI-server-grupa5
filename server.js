30762ac2cacf5f3cd318f3d1b2c641698eb9f59c
unknown <jdudic1@etf.unsa.ba>
2021-03-18 18:54:27 +0100

    Made server with some functions

--- server.js
+++ server.js
@@ -1,11 +1,36 @@
-const express = require('express')
-const app = express()
-const port = process.env.PORT || 8080
+// npm install uNetworking/uWebSockets.js#v18.14.0
+const { json } = require('express');
+const uWS = require('uWebSockets.js');
 
-app.get('/', (req, res) => {
-    res.send('Hello World!')
-})
+// an "app" is much like Express.js apps with URL routes,
+// here we handle WebSocket traffic on the wildcard "/*" route
+const app = uWS.App().ws('/*', {  // handle messages from client
 
-app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`)
-})
+  open: (socket, req) => {
+    /* For now we only have one canvas */
+    socket.send("Connected");
+    console.log("Someone connected");
+  },
+  message: (socket, message, isBinary) => {
+
+    var theMessage =JSON.parse(Buffer.from(message));
+
+    if(theMessage.key === 'subscribeToImage'){
+      socket.subscribe('imageReciever');
+      socket.send("subscribed");
+    }
+
+    else if(theMessage.key === 'image'){
+      app.publish("imageReciever",theMessage.value);
+    }
+
+    console.log("I got "+theMessage.key + " " +theMessage.value);
+    
+  }
+});
+
+app.listen(25565, (listenSocket) => {
+  if (listenSocket) {
+    console.log('Listening to port 25565');
+  }
+});
