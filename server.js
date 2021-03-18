0a2215d84b3c3b0e688f3b94f40cded148c1f055
unknown <jdudic1@etf.unsa.ba>
2021-03-18 21:59:42 +0100

    -

--- server.js
+++ server.js
@@ -1,12 +1,46 @@
-const http = require('http');
-const port = process.env.PORT || 3000
 
-const server = http.createServer((req, res) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/html');
-    res.end('<h1>Hello World</h1>');
+const { json } = require('express');
+const uWS = require('uWebSockets.js');
+
+
+
+const app = uWS.App().ws('/*', {  
+
+  open: (socket, req) => {
+    socket.send("Connected");
+    console.log("Someone connected "+socket.value);
+  },
+  message: (socket, message, isBinary) => {
+
+    var theMessage =JSON.parse(Buffer.from(message));
+
+    if(theMessage.key === 'subscribeToImage'){
+      socket.subscribe('imageReciever');
+      socket.send("subscribed");
+    }
+
+    else if(theMessage.key === 'image'){
+      app.publish("imageReciever",theMessage.value);
+    }
+
+    console.log("I got "+theMessage.key + " " +theMessage.value);
+    
+  }
+}).get('/', (res,req) => {
+
+  res.end("Nothing to see here");
+
 });
 
-server.listen(port, () => {
-    console.log(`Server running at port ` + port);
+
+
+var server_port = process.env.YOUR_PORT || process.env.PORT || 25565;
+var server_host = process.env.YOUR_HOST || '0.0.0.0';
+
+
+
+app.listen(server_port,server_host, () => {
+    console.log('Listening to port '+server_port);
+  
 });
+
