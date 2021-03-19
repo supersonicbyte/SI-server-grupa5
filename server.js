29bdd2d46d0966c066dbb160a32d1090db0bc3f6
unknown <jdudic1@etf.unsa.ba>
2021-03-19 10:27:19 +0100

    New socket code

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 
+/*
 const { json } = require('express');
 const uWS = require('uWebSockets.js');
+const cors = require('cors');
 
 
 
@@ -33,14 +35,38 @@ const app = uWS.App().ws('/*', {
 });
 
 
+*/
+
+const express = require('express');
+const app = express();
+const server = require('http').createServer(app);
+
+const cors = require('cors')
+
+const WebSocket = require('ws');
+
+const wss = new WebSocket.Server({server:server});
+
+app.use(cors());
+
+wss.on('connection', function connection(ws){
+
+console.log("CLient connected");
+ws.send("Connected");
+
+ws.on('message', function incoming(message){
+
+  console.log("Received "+message);
+
+})
+
+});
 
 var server_port = process.env.YOUR_PORT || process.env.PORT || 25565;
 var server_host = process.env.YOUR_HOST || '0.0.0.0';
 
 
+app.get('/', (req,res) => res.send("Hello world"));
 
-app.listen(server_port,server_host, () => {
-    console.log('Listening to port '+server_port);
-  
-});
+server.listen(server_port, () => console.log("Listening on port "+server_port))
 
