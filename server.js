ca833ba7a1ac63b9d133af511c8f8d262d7a1d91
unknown <jdudic1@etf.unsa.ba>
2021-03-21 20:45:21 +0100

    Response returns json now with answers

--- server.js
+++ server.js
@@ -21,6 +21,8 @@ const fetch = require('node-fetch');
 const clients = [];
 // map of responses {id, promise}
 const responseMap = [];
+// map of response messages//
+const messageMap = [];//
 
 wss.on('connection', function connection(ws) {
   ws.send("Connected");
@@ -32,43 +34,74 @@ wss.on('connection', function connection(ws) {
       responseMap[message.name + message.location] = emptyPromise(); 
     }
     else if (message.type === "command_result"){
-      const commandResponse = 
-      {
-        command_result: message.message,
-      }
-      responseMap[message.name + message.location].resolve(commandResponse);
+      messageMap[message.name + message.location].message = message.message;//
+      responseMap[message.name + message.location].resolve(messageMap[message.name + message.location]);
+    } else if (message.type === "sendScreenshot"){
+      messageMap[message.name + message.location].message = message.message;//
+      responseMap[message.name + message.location].resolve(messageMap[message.name + message.location]);
     }
   });
 
 });
 // validator for all /api routes, checks if token is valid
 app.use('/api', async function (req, res, next) {
+  const { name, location, command } = req.body;
   const authHeader = req.headers.authorization;
   const validation = await auth.validateJWT(authHeader);
   if (validation.status != 200) {
-    console.log("Radi");
     res.status(validation.status);
     res.send("Token not valid");
     return;
   }else{
     var x=await validation.json();
-  
-  }
-  next(x.accessToken)
-})
 
-/** POST request for executing command on remote machine
- * {
- *  name: "mirza-pc",
- *  location: "Sarajevo",
- *  command: "mkdir folder"
- * }
- */
+    const messageResponse = //
+      {
+        token: x.accessToken,//
+        message : ""//
+      }
+     messageMap[name+location] = messageResponse;//
+
+  }
+  
+  next();
+});
+
 app.post('/api/command', async (req, res) => {
   const { name, location, command } = req.body;
   let ws = clients[name + location];
+ // console.log("Dobio sam komandu "+name+" "+location+" "+command)
   if (ws !== undefined) {
     ws.send(command);
+    const errorTimeout = setTimeout(errFunction, 10000, name, location); 
+    responseMap[name + location].then((val) => {
+      clearTimeout(errorTimeout);
+      responseMap[name + location] = emptyPromise();
+     
+      res.json(val);
+    }).catch((err) => {
+      res.statusCode = 404;
+      res.json(err);
+    });
+  }
+  else {
+    var errResp = {
+      error: "Device is not connected to the server!",
+      name: name,
+      location: location
+    }
+    res.statusCode = 404;
+    res.json(errResp);
+  }
+});
+
+app.post('/api/screenshot', async (req, res) => {
+  const { name, location } = req.body;
+  let ws = clients[name + location];
+  //console.log("Dobio sam screen request "+name+" "+location);
+  if (ws !== undefined) {
+
+    ws.send("getScreenshot");
     const errorTimeout = setTimeout(errFunction, 10000, name, location); 
     responseMap[name + location].then((val) => {
       clearTimeout(errorTimeout);
@@ -105,5 +138,5 @@ function errFunction(name, location) {
 
 
 
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 25565;
 server.listen(PORT, () => console.log("Listening on port " + PORT));
