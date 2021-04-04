74ced1256661773ee0dd4d16d70e7eba5926b9c7
JasminDudic1 <56510794+JasminDudic1@users.noreply.github.com>
2021-04-04 15:03:18 +0200

    Uniq fix branch (#26)
    * Reworked unique system
    
    * Swagger update

--- auth/access_auth.js
+++ auth/access_auth.js
@@ -4,12 +4,12 @@ const fetch = require('node-fetch');
  * Validates token
  */
 async function validateUserAccess(token,deviceUid) {
-    const tokenResponse = fetch(process.env.AUTH_URL+deviceUid, {
+    let route = process.env.UNIQUE_URL+deviceUid;
+    const tokenResponse = fetch(route, {
         headers: { "Authorization": token }
     }).then(res => {
         return res;    
     });
-    
     return tokenResponse;
 }
 
