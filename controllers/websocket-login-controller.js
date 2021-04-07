d0d1cb1a52007203bd4b2942c7c4df42a9478ca1
Mirza Učanbarlić <56406159+tylerwetrust@users.noreply.github.com>
2021-04-07 16:30:22 +0200

    Refactorig and error handling (#35)
    * refactoring done
    
    * fixed bug

---
+++ controllers/websocket-login-controller.js
@@ -0,0 +1,21 @@
+const uniqueAuth = require('../unique/unique.js');
+const WebSocketService = require('../ws/websocket-service.js')
+
+async function websocketLogin(req, res, next) {
+    const uniqueId = req.body.id;
+    const validation = await uniqueAuth.validateUniqueCode(uniqueId);
+    if (validation.status != 200) {
+        res.status(validation.status);
+        res.send({ error: "Id not valid!" });
+        console.log("Id not valid for " + uniqueId);
+        return;
+    } else {
+        WebSocketService.addSession(uniqueId);
+        res.status(validation.status);
+        res.send({ message: 'Session updated' });
+        console.log("Session updated ");
+    }
+    next();
+}
+
+module.exports.websocketLogin = websocketLogin;
